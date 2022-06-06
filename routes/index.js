14c54de20252733668ba7bf891d7d7cab6b676bf
Nitya <nitya17nitya@gmail.com>
2022-06-06 23:22:30 +0530

    6 june(get and post)

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req)
-  res.json({name:"nitya"})
+   res.render('index')
 });
 
 module.exports = router;
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//const { rethrow } = require('jade/lib/runtime');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -7,9 +6,17 @@ router.get('/', function(req, res, next) {
     res.json({name:" ditya"})
 });
 
-router.get('/abc', function(req, res, next) {
-    console.log(req)
-    res.json({name:" ditya dwivedi"})
+router.get('/data', function(req, res, next) {
+    res.json({name:req.query.fname,lname:req.query.lname})
+});
+
+router.post('/abc', function(req, res, next) {
+    res.json({name:req.body.fname,lname:req.body.lname})
+});
+
+router.get('/:da-:ta', function(req, res, next) {
+        console.log(req.params)
+        res.json({name:req.params.da+","+req.params.ta+"="+req.params.da+req.params.ta})
 });
 
 module.exports = router;
